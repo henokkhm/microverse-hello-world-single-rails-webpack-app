595758f7ba85c6521713b76a3e2a52e92b2a7425
Henok Kirubel <henokhm2@gmail.com>
2024-02-03 13:32:00 +0300

    Set up store and Provider

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,12 +5,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import { store } from './redux/store.js';
 import App from './components/App';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
