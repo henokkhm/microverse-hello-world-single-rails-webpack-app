6cb5d740badc26e7c182c1b0d4b7d8a50c55f63e
Henok Kirubel <henokhm2@gmail.com>
2024-02-03 13:32:29 +0300

    Display greeting from redux store

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Greeting() {
-  return <div>Greeting</div>;
+  const { phrase, language, isLoading, isError } = useSelector(
+    (state) => state.greeting,
+  );
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Something went wrong. Please try reloading the page...</div>;
+  }
+
+  return (
+    <div>
+      <div>
+        <p>Phrase</p>
+        <p>{phrase}</p>
+      </div>
+      <div>
+        <p>Language</p>
+        <p>{language}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Greeting;
