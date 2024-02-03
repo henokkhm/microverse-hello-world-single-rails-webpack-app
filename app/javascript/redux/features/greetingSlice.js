d87cf31273acc789ac9862a46d59bd497e607dfd
Henok Kirubel <henokhm2@gmail.com>
2024-02-03 15:29:12 +0300

    Fetch greeting from backend in greetingsReducer

--- app/javascript/redux/features/greetingSlice.js
+++ app/javascript/redux/features/greetingSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
   phrase: 'Hello',
@@ -7,11 +7,41 @@ const initialState = {
   isError: true,
 };
 
+export const fetchGreeting = createAsyncThunk(
+  'greeting/fetchGreeting',
+  async () => {
+    try {
+      console.log('sending request to fetch greetings');
+      const response = await fetch('/greetings');
+      const data = await response.json();
+      console.log('fetched greeting');
+      console.log({ data });
+      return data.greeting;
+    } catch (error) {
+      throw new Error('Failed to fetch greeting');
+    }
+  },
+);
+
 const greetingSlice = createSlice({
   name: 'greeting',
   initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchGreeting.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchGreeting.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.language = action.payload.language;
+        state.phrase = action.payload.phrase;
+        state.isError = false;
+      })
+      .addCase(fetchGreeting.rejected, (state) => {
+        state.isLoading = false;
+        state.isError = true;
+      });
+  },
 });
-
-console.log(greetingSlice);
 
 export default greetingSlice.reducer;
