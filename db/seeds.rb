03d4d7ac79359fb0738bb33ffaf8e4a8136134df
Henok Kirubel <henokhm2@gmail.com>
2024-02-03 15:28:12 +0300

    Add more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,13 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-Greeting.create(language: 'English', phrase: 'Hello!')
 Greeting.create(language: 'Amharic', phrase: 'ሰላም!')
-Greeting.create(language: 'Somali', phrase: 'Asalaamu Caleykum!')
 Greeting.create(language: 'Swahili', phrase: 'Habari!')
 Greeting.create(language: 'Zulu', phrase: 'Sawubona!')
+Greeting.create(language: 'Hausa', phrase: 'Sannu!')
+Greeting.create(language: 'Somali', phrase: 'Salaam aleikum!')
+Greeting.create(language: 'Kinyarwanda', phrase: 'Muraho!')
+Greeting.create(language: 'Spanish', phrase: '¡Hola!')
+Greeting.create(language: 'German', phrase: 'Hallo!')
+Greeting.create(language: 'Portuguese', phrase: 'Olá!')
+Greeting.create(language: 'Russian', phrase: 'Привет!')
