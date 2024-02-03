dcf9a41877f1ae9432e9447c6c44740c0e8b1b5a
Henok Kirubel <henokhm2@gmail.com>
2024-02-03 15:29:47 +0300

    Add action and styles to Greeting component

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,29 +1,48 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { fetchGreeting } from '../redux/features/greetingSlice';
 
 function Greeting() {
   const { phrase, language, isLoading, isError } = useSelector(
     (state) => state.greeting,
   );
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, [dispatch]);
+  
+  const getRandomGreeting = () => {
+    dispatch(fetchGreeting());
+  };
+
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div className="wrapper">Loading...</div>;
   }
 
   if (isError) {
-    return <div>Something went wrong. Please try reloading the page...</div>;
+    return (
+      <div className="wrapper">
+        Something went wrong. Please try reloading the page...
+      </div>
+    );
   }
 
   return (
-    <div>
-      <div>
-        <p>Phrase</p>
-        <p>{phrase}</p>
-      </div>
-      <div>
-        <p>Language</p>
-        <p>{language}</p>
-      </div>
+    <div className="wrapper">
+      <p className="language">
+        <span>"Hello" in</span>
+        <span className="language_name">{language}</span>
+      </p>
+      <div className="greeting">{phrase}</div>
+      <button
+        className="btn"
+        onClick={getRandomGreeting}
+      >
+        Randomize
+      </button>
     </div>
   );
 }
