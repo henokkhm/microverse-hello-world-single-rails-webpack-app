12bee78484c262fae895232b72265dd5b07010d6
Henok Kirubel <henokhm2@gmail.com>
2024-02-03 15:28:04 +0300

    Add root controller and adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root 'greetings#index'
+  root 'root#index'
+
+  get '/greetings', to: 'greetings#index'
 end
